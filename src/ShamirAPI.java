ca33293b89a8c48857c69fa4a8094b821d2eee3e
markd315 <markd315@gmail.com>
2017-12-14 21:09:11 -0500

    implemented the split API method

--- src/ShamirAPI.java
+++ src/ShamirAPI.java
@@ -1,19 +1,29 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ShamirAPI {
-	static int prime = 1237;
-
+	static BigInteger prime = new BigInteger(
+			"1040793219466439908192524032736408553861526224726670480531911235040360805967336029801223944173232418" + 
+			"4842421613954281007791383566248323464908139906605677320762924129509389220345773183349661583550472959" + 
+			"4205476898112116936771475484788669625013844382602917323488853111608285384165850282556046662248318909" + 
+			"18801847068222203140521026698435488732958028878050869736186900714720710555703168729087");
+	//386 decimal digits == 1278 bits of random number generation. Perfectly sufficient.
+	
 	/* Split number into the shares */
 	public static BigInteger[] split(BigInteger number, int available, int needed) {
 		BigInteger[] coef = new BigInteger[needed]; //2 points define a line: takes ax +b, 3 points define a parabola: takes ax^2 + bx + c
 	    //TODO Populate coef with number as LSB and random generated info for higher order.
-		BigInteger[] coef = {number, 166, 94};
-	    
-	    int x, exp, c, accum; 
-	    BigInteger[] shares = new BigInteger[available]; //TODO 
+		coef[0] = number;
+		
+		for(int c = 1; c < needed; c++)
+			coef[c] = new BigInteger(1024, new Random());
+			//TODO previous line will fail. Can't multiply double by bigint
+	    int x, exp, c;
+		BigInteger y;
+	    BigInteger[] shares = new BigInteger[available * 2]; //TODO 
 	    /* Normally, we use the line:
-	     * for(c = 1, coef[0] = number; c < needed; c++) coef[c] = Math.floor(Math.random() * (prime  - 1));
+	     * 
 	     * where (prime - 1) is the maximum allowable value.
 	     * However, to follow this example, we hardcode the values:
 	     * coef = [number, 166, 94];
@@ -21,11 +31,17 @@ public class ShamirAPI {
 	     * For each share that is requested to be available, run through the formula plugging the corresponding coefficient
 	     * The result is f(x), where x is the byte we are sharing (in the example, 1234)
 	     */
-	    for(x = 1; x <= available; x++) {
+	    for(x = 0; x < available;) {
 	        /* coef = [1234, 166, 94] which is 1234x^0 + 166x^1 + 94x^2 */
-	        for(exp = 1, accum = coef[0]; exp < needed; exp++) accum = (accum + (coef[exp] * (Math.pow(x, exp) % prime) % prime)) % prime;
-	        /* Store values as (1, 1155), (2, 781), (3, 112), (4, 385), (5, 363) (6, 46) */
-	        shares[x - 1] = [x, accum];
+	    	y = coef[0];
+	    	for(exp = 1; exp < needed; exp++) {
+	    		int xpow = (int) Math.pow(x, exp);
+	        	y = coef[exp].multiply(BigInteger.valueOf(xpow)).add(y);// y+= coef * (x^exp)
+	    	}
+	        /* Store values as tuples like (1, 1155), (2, 781), (3, 112), (4, 385), (5, 363) (6, 46) */
+	        shares[x] = BigInteger.valueOf(x);
+	        shares[x+1] = y;
+	        x+=2;
 	    }
 	    return shares;
 	}
@@ -42,7 +58,7 @@ public class ShamirAPI {
 
 	/* Gives the multiplicative inverse of k mod prime.  In other words (k * modInverse(k)) % prime = 1 for all prime > k >= 1  */
 	public static BigInteger modInverse(BigInteger k) { 
-	    k = k % prime;
+	    k = k.mod(BigInteger.valueOf(prime)); // k%=prime
 	    BigInteger r = (k < 0) ? - gcdD(prime,-k)[2] : gcdD(prime,k)[2];
 	    return (prime + r) % prime;
 	}
